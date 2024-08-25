8df8a9de103dafa0da4f68d0c726c4c78d6e56c5
chenxing <1057791979@qq.com>
2024-08-25 19:46:50 +0800

    泛型桥接方法示例注释更新

--- src/bridginginheritance/Son.java
+++ src/bridginginheritance/Son.java
@@ -10,12 +10,30 @@ public class Son extends Parent<Pojo> {
 
     public static void main(String[] args) throws NoSuchMethodException {
         /*
-         * testBridging  [class bridginginheritance.Pojo]
-         * testBridging  [class java.lang.Object]   重写的方法
-         * main  [class [Ljava.lang.String;]
-         * lambda$main$0  [class java.lang.reflect.Method]
+        使用javap反编译的Son类
+        public class bridginginheritance.Son extends bridginginheritance.Parent<bridginginheritance.Pojo> {
+          public bridginginheritance.Son();
+          protected void testBridging(bridginginheritance.Pojo);
+          public static void main(java.lang.String[]) throws java.lang.NoSuchMethodException;
+          protected void testBridging(java.lang.Object);
+        }
+        javap -c 反编译的结果
+          protected void testBridging(java.lang.Object);
+          protected void testBridging(java.lang.Object);
+            Code:
+               0: aload_0
+               1: aload_1
+            Code:
+               0: aload_0
+               1: aload_1
+               2: checkcast     #35                 // class bridginginheritance/Pojo
+               5: invokevirtual #37                 // Method testBridging:(Lbridginginheritance/Pojo;)V
+               8: return
+        }
+
+        可以看到实际上是在testBridging(java.lang.Object)中通过invokevirtual调用了Method testBridging:(Lbridginginheritance/Pojo;)
          */
 //        Son在重写testBridging方法的时候，由于父类泛型的类型擦除，实际重写的是参数为Object的方法，在该方法内部调用参数为Pojo的方法
-        Arrays.stream(Son.class.getDeclaredMethods()).forEach(t -> System.out.println(t.getName() + "  " +  Arrays.toString(t.getGenericParameterTypes())));
+        Arrays.stream(Son.class.getDeclaredMethods()).forEach(t -> System.out.println(t.getName() + "  " + Arrays.toString(t.getGenericParameterTypes())));
     }
 }
